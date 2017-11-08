4ff80a941fe479a1f0cdd943f143446982971fbb
Sam Smith <smithrunner@live.com>
2017-11-07 20:34:30 -0600

    blocmetrics added to track about page clicks

---
+++ scripts/blocmetrics.js
@@ -0,0 +1,13 @@
+var blocmetrics = {};
+
+blocmetrics.report = function(eventName){
+  var event = {event: {name: eventName}};
+
+  var request = new XMLHttpRequest();
+
+  request.open("POST", "https://blocmetrics-smithrunner.c9users.io/api/events", true);
+
+  request.setRequestHeader('Content-Type', 'application/json');
+
+  request.send(JSON.stringify(event));
+};
